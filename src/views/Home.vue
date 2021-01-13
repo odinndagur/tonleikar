b52fc9ad2d1a48cfb9b223a9fe3acf0a0252bc34
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2021-01-13 22:48:43 +0000

    added sorting by date

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="home">
-
-  <input type="checkbox" id="filterArtists" name="filterArtists" v-model="filterArtists">
+    <input
+      type="checkbox"
+      id="filterArtists"
+      name="filterArtists"
+      v-model="filterArtists"
+    />
     <label for="filterArtists">Filter by artist</label>
 
     <select v-model="selectedArtist">
@@ -16,21 +20,21 @@
 
     <br />
 
-    <input type="checkbox" id="filterVenues" name="filterVenues" v-model="filterVenues">
+    <input
+      type="checkbox"
+      id="filterVenues"
+      name="filterVenues"
+      v-model="filterVenues"
+    />
     <label for="filterVenues">Filter by venue</label>
 
-        <select v-model="selectedVenue">
-      <option
-        v-for="venue in noDuplicatesVenues"
-        :key="venue"
-        :value="venue"
-      >
+    <select v-model="selectedVenue">
+      <option v-for="venue in noDuplicatesVenues" :key="venue" :value="venue">
         {{ venue }}
       </option>
     </select>
 
-
-    <SetCard v-for="set in filtered" :key="set.videoId" :set="set" /> <br />
+    <SetCard v-for="set in filtered" :key="set" :set="set" /> <br />
   </div>
 </template>
 
@@ -62,18 +66,42 @@ export default {
       if (this.filterVenues) {
         tempArr = tempArr.filter((set) => set.venue == this.selectedVenue);
       }
-      return tempArr.sort((a,b)=>{new Date(a.date) - new Date(b.date)});
+
+      // return tempArr.sort();
+      return tempArr.sort((a, b) => {
+        let dateA,dateB;
+        if(a.date.length <= 4){
+          dateA = '' + dateA + '-' + '01-01T00:00';
+        }
+                if(b.date.length <= 4){
+          dateB = '' + dateB + '-' + '01-01T00:00';
+        }
+        else{
+        dateA = a.date.split(".");
+        dateB = b.date.split(".");
+        if(!dateA[2]){
+          dateA[1] = '01';
+          dateA[2] = '01';
+          dateB[1] = '01';
+          dateB[2] = '01';
+        }
+        dateA = "" + dateA[2] + "-" + dateA[1] + "-" + dateA[0] + "T00:00";
+        dateB = "" + dateB[2] + "-" + dateB[1] + "-" + dateB[0] + "T00:00";
+        }
+        return new Date(dateB) - new Date(dateA);
+      });
     },
-    noDuplicatesArtists(){
-            let arr = [];
-      this.sets.forEach(set => arr.push(set.artist));
+    noDuplicatesArtists() {
+      let arr = [];
+      this.sets.forEach((set) => arr.push(set.artist));
       return [...new Set(arr)].sort();
     },
-        noDuplicatesVenues(){
-            let arr = [];
-      this.sets.forEach(set => arr.push(set.venue));
+    noDuplicatesVenues() {
+      let arr = [];
+      this.sets.forEach((set) => arr.push(set.venue));
       return [...new Set(arr)].sort();
-    }
+    },
+    
   },
 };
 </script>
